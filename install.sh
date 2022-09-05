77cf94669781956b316da072c0e7b8f2ea2ab25b
Giuseppe Attanasio <giuseppeattanasio6@gmail.com>
2022-09-05 12:59:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,11 +7,11 @@ pip install -r ./requirements.txt
 
 # download spacy language support∫
 python -m spacy download en
-python -m spacy download de
+# python -m spacy download de
 python -m spacy download it
-python -m spacy download es
-python -m spacy download fr
+# python -m spacy download es
+# python -m spacy download fr
 
 # Czech morphology annotation
-curl --remote-name-all https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-1836{/czech-morfflex-pdt-161115.zip}
-unzip czech-morfflex-pdt-161115.zip
+# curl --remote-name-all https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-1836{/czech-morfflex-pdt-161115.zip}
+# unzip czech-morfflex-pdt-161115.zip
